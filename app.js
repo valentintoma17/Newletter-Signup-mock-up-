ec3a9b143ffb2bc4b2fae035d7115d74abd4b8ee
valentintoma17 <73826499+valentintoma17@users.noreply.github.com>
2022-02-07 18:39:18 +0200

    redone commit

--- app.js
+++ app.js
@@ -1,80 +1,66 @@
 const express = require("express");
-const request = require("request");
 const https = require("https");
-// const bodyParser = require('body-parser')
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended:true}));
-// app.use(bodyParser.urlencoded({ extended:true}));
+app.use(express.urlencoded({extended:true}));
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname + "/signup.html");
+app.get("/", function (req, res) {
+   res.sendFile(__dirname + "/signup.html");
 });
 
 app.post("/", function(req, res){
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const surname = req.body.sName;
+  const email = req.body.email;
 
-  var data = {
-    members: [
+  const data ={
+    members:[
       {email_address: email,
       status: "subscribed",
-      merge_fields: {
+      merg_fields:{
         FNAME: firstName,
-        LNAME: lastName
+        LNAME: surname
       }}
     ]
   };
 
-  const jsonData = JSON.stringify(data);
+  const jsonData= JSON.stringify(data);
 
   const url = "https://us20.api.mailchimp.com/3.0/lists/69f3df4689";
-
-  // https://us6.api.mailchimp.com/3.0/
-
   const options = {
-    method: "POST",
+    method: 'POST',
     headers: {
-              Authorization: 'Bearer a142b60281dfee8039ddc68952cdf243-us20'
-          }
+             Authorization: 'Bearer a142b60281dfee8039ddc68952cdf243-us20'
+         }
   };
-  // const options = {
-  //       method: "POST",
-  //       headers: {
-  //           Authorization: 'XXXXX: your APIkey'
-  //       }
-  console.log(jsonData);
 
   const request = https.request(url, options, function(response){
 
-    if(response.statusCode === 200){
+    if (response.statusCode === 200){
       res.sendFile(__dirname + "/success.html");
     }else {
-      res.sendFile(__dirname + "/failure.html")
+      res.sendFile(__dirname + "/failure.html");
     }
 
     response.on("data", function(data){
       console.log(JSON.parse(data));
-    });
+    })
   });
-request.write(jsonData);
-request.end();
 
+  request.write(jsonData);
+  request.end();
 });
 
 app.post("/failure", function(req, res){
-  res.redirect("/");
+  res.redirect("/")
 });
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Server is running at port 3000.");
+   console.log("Server is running on port 3000.");
 });
 
-// API KEy
-// ac3ba8cbf231be401b6576aacad4bb64-us20
+// API key : a142b60281dfee8039ddc68952cdf243-us20
 
-// AudienceID
-// 69f3df4689
+// audience unique id: 69f3df4689
